a60f95595a3559fd202a49b8e89c7a03988e5e3b
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-08-15 13:16:06 -0400

    Remove premature System.Reflection global using from IntegrationEventLogEF

--- src/IntegrationEventLogEF/GlobalUsings.cs
+++ src/IntegrationEventLogEF/GlobalUsings.cs
@@ -1,5 +1,4 @@
 global using System.ComponentModel.DataAnnotations.Schema;
-global using System.Reflection;
 global using System.Text.Json;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Storage;
